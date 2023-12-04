fb55b89a33b5801ded7e3878b59a578b8d8d7387
Agus Narvaez <agus.narvaez@outlook.com>
2023-12-04 15:34:16 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+//  Módulos angular
+import { NgModule } from '@angular/core'
+import { AppComponent } from './app.component'
+/* import { AppRoutingModule } from './app-routing.module' */
+
+// Componentes
+
+// Páginas
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AppComponent
+    /* FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterOutlet,
+    RouterModule,
+    HttpClientModule,
+    NgOptimizedImage */
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule {}
