174654406f851675eafc370bf410faf15109c102
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 01:03:38 -0300

    Creo componente home-contact

---
+++ src/app/sections/home/home-contact/home-contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-home-contact',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-contact.component.html',
+  styleUrl: './home-contact.component.css'
+})
+export class HomeContactComponent {
+
+}
