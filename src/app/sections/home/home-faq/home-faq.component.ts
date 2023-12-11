147e9f5f0d81a3dff90f9c36fd59b974fe08f3cf
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 01:01:55 -0300

    Cambio mock de preguntas frecuentes

--- src/app/sections/home/home-faq/home-faq.component.ts
+++ src/app/sections/home/home-faq/home-faq.component.ts
@@ -13,32 +13,27 @@ export class HomeFaqComponent {
   constructor() {}
   faqList = [
     {
-      question: 'What is the best way to get started?',
+      question: '¿Qué documentación necesito para poder publicar mi propiedad?',
+      answer:
+        'Título de propiedad, declaratoria de herederos o poder DNI de los titulares, o apoderados Reglamento de copropiedad (en caso de departamentos o PH) Plano Liquidación de expensas Impuesto municipal COTI'
+    },
+    {
+      question: '¿Cuáles son los gastos estimados en una operación de compraventa?',
       answer:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
     },
     {
-      question: 'What is the best way to get started?',
+      question: '¿Cuál es la inversión inicial para un alquiler?',
       answer:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
     },
     {
-      question: 'What is the best way to get started?',
+      question: '¿Cómo calculo el índice de ajuste de mi alquiler?',
       answer:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
     },
     {
-      question: 'What is the best way to get started?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
-    },
-    {
-      question: 'What is the best way to get started?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
-    },
-    {
-      question: 'What is the best way to get started?',
+      question: '¿Cómo preparo mi casa para el relevamiento multimedia y las visitas?',
       answer:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
     }
