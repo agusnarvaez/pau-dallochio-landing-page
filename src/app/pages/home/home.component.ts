f38a2b8b4f7cf4d3798d2b0af45686e5ca2d2275
agusnarvaez <agus.narvaez@outlook.com>
2023-12-10 22:24:10 -0300

    Agrego componentes a home y separador

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeBannerComponent } from '../../sections/home/home-banner/home-banner.component'
-import { HomeIntroductionComponent } from '../../sections/home/home-introduction/home-introduction.component'
-import { HomeProductsComponent } from '../../sections/home/home-products/home-products.component'
-import { HomeDevelopmentComponent } from '../../sections/home/home-development/home-development.component'
-import { HomeSellersComponent } from '../../sections/home/home-sellers/home-sellers.component'
 import { HomeFaqComponent } from '../../sections/home/home-faq/home-faq.component'
+import { HomeStudioComponent } from '../../sections/home/home-studio/home-studio.component'
+import { HomeReviewsComponent } from '../../sections/home/home-reviews/home-reviews.component'
 
 @Component({
   selector: 'app-home',
@@ -13,11 +11,8 @@ import { HomeFaqComponent } from '../../sections/home/home-faq/home-faq.componen
   imports: [
     CommonModule,
     HomeBannerComponent,
-    HomeIntroductionComponent,
-    HomeBannerComponent,
-    HomeProductsComponent,
-    HomeDevelopmentComponent,
-    HomeSellersComponent,
+    HomeStudioComponent,
+    HomeReviewsComponent,
     HomeFaqComponent
   ],
   templateUrl: './home.component.html',
