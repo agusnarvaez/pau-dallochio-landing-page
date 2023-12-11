4a291739275c37c7d8c1508ab2613b4ea8c50422
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 01:20:38 -0300

    Importo botón

--- src/app/sections/home/home-contact/home-contact.component.ts
+++ src/app/sections/home/home-contact/home-contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../../components/button/button.component'
 
 @Component({
   selector: 'app-home-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './home-contact.component.html',
   styleUrl: './home-contact.component.css'
 })
