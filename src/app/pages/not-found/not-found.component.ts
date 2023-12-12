ff9f12c25055790f11288baadc6cbdca3b70d1cb
agusnarvaez <agus.narvaez@outlook.com>
2023-12-12 08:30:00 -0300

    Agrego botón a not-found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../components/button/button.component'
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
