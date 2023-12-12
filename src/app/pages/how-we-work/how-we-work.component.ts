3f8e4dad4293731e909e4a21cf0577ec276ef916
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 21:34:13 -0300

    Agrego estructura y componentes de Cómo Trabajamos

--- src/app/pages/how-we-work/how-we-work.component.ts
+++ src/app/pages/how-we-work/how-we-work.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { PurposeComponent } from '../../sections/how-we-work/purpose/purpose.component'
+import { SalesComponent } from '../../sections/how-we-work/sales/sales.component'
+import { AdviceComponent } from '../../sections/how-we-work/advice/advice.component'
 
 @Component({
   selector: 'app-how-we-work',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,PurposeComponent,SalesComponent,AdviceComponent],
   templateUrl: './how-we-work.component.html',
   styleUrl: './how-we-work.component.css'
 })
