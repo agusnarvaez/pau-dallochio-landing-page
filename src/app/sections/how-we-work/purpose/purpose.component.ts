6e89e1c2d192b96505d6776d7450c548a2b7ad84
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 21:35:35 -0300

    Agrego componente de sección Propósito

---
+++ src/app/sections/how-we-work/purpose/purpose.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-purpose',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './purpose.component.html',
+  styleUrl: './purpose.component.css'
+})
+export class PurposeComponent {
+
+}
