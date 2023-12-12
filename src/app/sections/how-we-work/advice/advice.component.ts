381b34852d448249a56f1fd18055ef29b6566d43
agusnarvaez <agus.narvaez@outlook.com>
2023-12-11 21:36:20 -0300

    Agrego componente de Consejos

---
+++ src/app/sections/how-we-work/advice/advice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-advice',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './advice.component.html',
+  styleUrl: './advice.component.css'
+})
+export class AdviceComponent {
+
+}
