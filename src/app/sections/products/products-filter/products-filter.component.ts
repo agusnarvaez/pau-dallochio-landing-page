6e6c5ea3a32763aeba41fff1d108294608d21c09
agusnarvaez <agus.narvaez@outlook.com>
2023-12-12 16:29:39 -0300

    Agrego filtro con breve interacción

--- src/app/sections/products/products-filter/products-filter.component.ts
+++ src/app/sections/products/products-filter/products-filter.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../../components/button/button.component'
 
 @Component({
   selector: 'app-products-filter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './products-filter.component.html',
   styleUrl: './products-filter.component.css'
 })
 export class ProductsFilterComponent {
+  filter = 'Comprar'
 
+  isFilterActive = (filterText:string) =>  this.filter != filterText
+
+  setActiveFilter = (filterText:string) => this.filter = filterText
+  
 }
