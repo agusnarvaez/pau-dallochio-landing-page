a0d34670e2be2568a0653a8a0c99e80a181bdf88
agusnarvaez <agus.narvaez@outlook.com>
2023-12-13 13:56:08 -0300

    Corrijo respuestas de FAQ

--- src/app/sections/home/home-faq/home-faq.component.ts
+++ src/app/sections/home/home-faq/home-faq.component.ts
@@ -15,27 +15,26 @@ export class HomeFaqComponent {
     {
       question: '¿Qué documentación necesito para poder publicar mi propiedad?',
       answer:
-        'Título de propiedad, declaratoria de herederos o poder DNI de los titulares, o apoderados Reglamento de copropiedad (en caso de departamentos o PH) Plano Liquidación de expensas Impuesto municipal COTI'
+        'PARA OPERACIONES DE VENTA: <br>• Título de propiedad, declaratoria de herederos o poder DNI de los titulares, o apoderados <br>• Reglamento de copropiedad (en caso de departamentos o PH) <br>• Plano <br> • Liquidación de expensas <br>• Impuesto municipal <br>• COTI'
     },
     {
       question: '¿Cuáles son los gastos estimados en una operación de compraventa?',
       answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+        'PARA OPERACIONES DE VENTA: <br> • IMPUESTOS: (se abonan en pesos al equivalente del valor del dólar oficial) Retiene el escribano actuante. Se liquidan sobre el valor de escrituración . Sellos 1.75% (por parte), I.T.I. (en caso de corresponder) 1,5%, IVA. <br> • GASTOS DE ESCRIBANÍA: 1-2% (Se solicita presupuesto al escribano actuante) <br> • HONORARIOS INMOBILIARIA: 3%  <br> • IVA (cliente vendedor); 4% <br> • IVA (cliente comprador) <br> • HONORARIOS ESCRIBANÍA: Solo le corresponden al cliente comprador'
     },
     {
       question: '¿Cuál es la inversión inicial para un alquiler?',
       answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+        'PARA OPERACIONES DE ALQUILER RESIDENCIAL PERMANENTE: <br><br> Honorarios inmobiliaria: 4.15% del total del alquiler (Se calcula con el valor del primer mes multiplicado por 36 meses de la duración mínima del contrato.  Los abona la parte LOCATARIA). <br><br> Certificación de firma por escribano'
     },
     {
       question: '¿Cómo calculo el índice de ajuste de mi alquiler?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+      answer: '<a href="https://cpicalq.portalcucicba.com.ar/calculo_add.php" target="_blank" rel="noreferer" >CALCULADORA DE AJUSTE</a>'
     },
     {
       question: '¿Cómo preparo mi casa para el relevamiento multimedia y las visitas?',
       answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+        'El Home staging o puesta en escena, es la forma en que se prepara tu inmueble para que se luzca en la publicación y las visitas de los interesados.  Sugerimos: <br> •	Despejar muebles y ambientes <br>•	Despersonalizar muros, quitar fotos e imágenes religiosas <br>•	Mejorar el atractivo exterior <br>•	Darle estilo a la mesa del comedor <br>•	Generar un espacio de relax al aire libre <br>•	Prestar atención a la estación del año <br>•	Limpiar y ventilar <br>•	Guardar cualquier juguete u objeto que pueda estar sobre el piso <br>•	Aromatizar sutilmente'
     }
   ]
 
