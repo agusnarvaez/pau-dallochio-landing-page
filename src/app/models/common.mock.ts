62ff9f86b72a0ba9b8a6f875f02d08584d210de8
agusnarvaez <agus.narvaez@outlook.com>
2023-12-13 10:11:24 -0300

    Creo mock

---
+++ src/app/models/common.mock.ts
@@ -0,0 +1,26 @@
+import { Product } from "./product"
+import { Address } from './address'
+
+
+export const address = new Address(
+    "Malabia",
+    "1000",
+    "Palermo",
+    "CABA",
+    "Buenos Aires"
+)
+
+export const product = new Product(
+    "Departamento",
+    address,
+    350000,
+    100,
+    80,
+    3,
+    3,
+    ['../../../assets/backgrounds/stock-image.jpg']
+)
+
+export const productsMock = [
+    product, product,product
+]
