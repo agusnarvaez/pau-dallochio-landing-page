3996276b4b49a2c66e264a01bdda00a67eec8d9e
agusnarvaez <agus.narvaez@outlook.com>
2023-12-14 20:11:20 -0300

    Agrego filtro a home banner

--- src/app/sections/home/home-banner/home-banner.component.ts
+++ src/app/sections/home/home-banner/home-banner.component.ts
@@ -1,13 +1,22 @@
+import { ProductService } from './../../../services/product/product.service'
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../../components/button/button.component'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './home-banner.component.html',
   styleUrl: './home-banner.component.css'
 })
 export class HomeBannerComponent {
+  constructor(private router: Router, private productService: ProductService) {}
 
+  isFilterActive = (filterText: string): boolean => this.productService.filters().includes(filterText)
+
+  toggleFilter = (filterText: string): void => this.productService.filtersService.toggle(filterText)
+
+  searchProducts = () => this.router.navigate(['/catalogo'])
 }
