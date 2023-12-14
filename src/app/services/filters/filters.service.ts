f547fcff32f82c13d9d3b82410b0c9e84b9906e3
agusnarvaez <agus.narvaez@outlook.com>
2023-12-14 20:11:03 -0300

    Agrego toggle en filter service

--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -11,9 +11,11 @@ export class FiltersService {
 
   get = () => this._filters
 
-  add = (filter: string) => this._filters.push(filter)
+  add = (filter: string) => {this._filters.push(filter)}
 
-  remove = (filter: string) => this._filters = this._filters.filter(f => f !== filter)
+  remove = (filter: string) => {this._filters = this._filters.filter(f => f !== filter)}
 
-  clear = () => this._filters = []
+  clear = () => {this._filters = []}
+
+  toggle = (filter: string) => {this._filters.includes(filter) ? this.remove(filter) : this.add(filter)}
 }
