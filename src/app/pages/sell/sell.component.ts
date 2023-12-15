7617731d5981a1119e4d6c5dc0f7f1828cad0fce
agusnarvaez <agus.narvaez@outlook.com>
2023-12-14 21:30:46 -0300

    Agrego componente de sección Sell

---
+++ src/app/pages/sell/sell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-sell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sell.component.html',
+  styleUrl: './sell.component.css'
+})
+export class SellComponent {
+
+}
