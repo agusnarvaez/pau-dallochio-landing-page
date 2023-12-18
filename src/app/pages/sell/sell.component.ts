e6295b134b6d395dbaa746fc01e89d858afa55a1
agusnarvaez <agus.narvaez@outlook.com>
2023-12-17 22:38:16 -0300

    Agrego estructura de vender

--- src/app/pages/sell/sell.component.ts
+++ src/app/pages/sell/sell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../components/button/button.component'
 
 @Component({
   selector: 'app-sell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent],
   templateUrl: './sell.component.html',
   styleUrl: './sell.component.css'
 })
