1cbf35a2f442dc0aec0172454528bec850024aaa
agusnarvaez <agus.narvaez@outlook.com>
2023-12-19 22:40:46 -0300

    Corrijo clase dirección

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -2,15 +2,9 @@
 
 export class Address{
     street: string
-    number: string
-    locality: string
     city: string
-    province: string
-    constructor(street: string, number: string, locality: string, city: string, province: string){
+    constructor(street: string, city: string){
         this.street = street
-        this.number = number
-        this.locality = locality
         this.city = city
-        this.province = province
     }
 }
