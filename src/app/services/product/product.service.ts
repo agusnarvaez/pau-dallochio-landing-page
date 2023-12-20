ecf6f5cef01c8c3eaf97a2c7e6e98595b8c86970
agusnarvaez <agus.narvaez@outlook.com>
2023-12-20 00:26:18 -0300

    Agrego petición de detalle de propiedad

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core'
 import { productsMock } from '../../models/common.mock'
 import { FiltersService } from '../filters/filters.service'
 import { Observable, catchError, map,throwError } from 'rxjs'
-import { ProductList } from '../../models/product'
+import { ProductDetail, ProductList } from '../../models/product'
 import { HttpClient } from '@angular/common/http'
 import { enviroment } from '../../../../enviroment.prod'
 
@@ -20,8 +20,24 @@ interface ProductListResponse {
   total_surface: number
   roofed_surface: number
   room_amount: number
+  rich_description: string
   bathroom_amount: number
   photos: { image: string }[]
+  geo_lat: number
+  geo_long: number
+  floors_amount: number
+  location: { name: string }
+  parking_lot_amount: number
+  property_condition:string
+  publication_title:string
+  situation:string
+  tags:[
+    {
+      id:number,
+      name:string
+      type:number
+    }
+  ]
 }
 
 @Injectable({
@@ -57,7 +73,7 @@ export class ProductService {
                     p.type.name,
                     new Address(
                       p.address,
-                      p.branch.address
+                      p.location.name,
                       ),
                       p.operations[0].prices[0].price,
                       p.operations[0].prices[0].currency,
@@ -70,8 +86,42 @@ export class ProductService {
                 })
               }))
   }
-
-  getById = (id: string) => productsMock.find(p => p.id === id)
+  /* => productsMock.find(p => p.id === id) */
+  getById(id: string):Observable<ProductDetail> {
+    return this.http
+            .get<ProductListResponse>(`https://www.tokkobroker.com/api/v1/property/${id}/?lang=es_ar&key=${enviroment.tokkoBrokerKey}`)
+            .pipe(
+              catchError(error => {
+                return throwError(() => new Error(error.message))
+              }),
+              map((response) => {
+                // Aquí puedes hacer alguna transformación de los datos si es necesario.
+                return new ProductDetail(
+                  response.id,
+                  response.publication_title,
+                  response.type.name,
+                  new Address(
+                    response.address,
+                    response.location.name,
+                    ),
+                    response.geo_lat,
+                    response.geo_long,
+                    response.operations[0].prices[0].price,
+                    response.operations[0].prices[0].currency,
+                  response.total_surface,
+                  response.roofed_surface,
+                  response.room_amount,
+                  response.bathroom_amount,
+                  response.parking_lot_amount,
+                  response.photos.map((photo) => photo.image),
+                  response.rich_description,
+                  /* response.floors_amount,
+                  response.tags.map((tag) => tag.name),
+                  response.situation,
+                  response.property_condition */
+                )
+              }))
+  }
 
   getSuggested = (id:string) => {
     console.log(id)
