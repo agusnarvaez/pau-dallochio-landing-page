9bd71cb80247706ba7ef3888c69739e1732dc3f1
agusnarvaez <agus.narvaez@outlook.com>
2023-12-20 00:24:08 -0300

    Agrego datos necesarios a modelo de Product Detail

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -29,23 +29,51 @@ export class ProductList{
 export class ProductDetail{
     id: string
     type: string
+    title: string
     address: Address
     price: number
+    currency: string
     area: number
     coveredArea: number
     rooms: number
     bathrooms: number
+    garage: number
     images: string[]
+    description: string
+    geo_lat: number
+    geo_long: number
 
-    constructor(id: string, type: string, address: Address, price: number, area: number, coveredArea: number, rooms: number, bathrooms: number, images: string[]){
+    constructor(
+        id: string,
+        title:string,
+        type: string,
+        address: Address,
+        geo_lat: number,
+        geo_long: number,
+        price: number,
+        currency:string,
+        area: number,
+        coveredArea: number,
+        rooms: number,
+        bathrooms: number,
+        garage: number,
+        images: string[],
+        description: string
+        ){
         this.id = id
+        this.title = title
         this.type = type
         this.address = address
+        this.geo_lat = geo_lat
+        this.geo_long = geo_long
         this.price = price
+        this.currency = currency
         this.area = area
         this.coveredArea = coveredArea
         this.rooms = rooms
         this.bathrooms = bathrooms
+        this.garage = garage
         this.images = images
+        this.description = description
     }
 }
