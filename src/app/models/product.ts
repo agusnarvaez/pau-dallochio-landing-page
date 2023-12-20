65879f970ef0538b7e3158af1f7fff4be191db27
agusnarvaez <agus.narvaez@outlook.com>
2023-12-19 22:41:02 -0300

    Corrijo clase ProductList

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,32 @@
 import { Address } from './address'
 
-export class Product{
+export class ProductList{
+    id: string
+    type: string
+    address: Address
+    price: number
+    area: number
+    coveredArea: number
+    rooms: number
+    bathrooms: number
+    images: string[]
+    currency: string
+
+    constructor(id: string, type: string, address: Address, price: number, currency: string, area: number, coveredArea: number, rooms: number, bathrooms: number, images: string[]){
+        this.id = id
+        this.type = type
+        this.address = address
+        this.price = price
+        this.currency = currency
+        this.area = area
+        this.coveredArea = coveredArea
+        this.rooms = rooms
+        this.bathrooms = bathrooms
+        this.images = images
+    }
+}
+
+export class ProductDetail{
     id: string
     type: string
     address: Address
