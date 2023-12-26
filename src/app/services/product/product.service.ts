f553d0e5322945742c1615e73248f7adc418f4a0
agusnarvaez <agus.narvaez@outlook.com>
2023-12-26 19:48:41 -0300

    Agrego implementación de sanity en product service

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core'
-import { productsMock } from '../../models/common.mock'
 import { FiltersService } from '../filters/filters.service'
 import { Observable, catchError, forkJoin, map,throwError } from 'rxjs'
 import { Product, SanityProduct, TokkoProduct } from '../../models/product'
@@ -35,19 +34,19 @@ export class ProductService {
     return forkJoin({
       tokko: this.getAllFromTokko(),
       sanity: this.getAllFromSanity()
-  }).pipe(
-      map(results => {
-          // Combinar los resultados de Tokko y Sanity
-          return [...results.tokko, ...results.sanity]
-      }),
-      catchError(error => {
-          // Manejar el error
-          return throwError(() => new Error(error.message))
-      })
-  )
+    }).pipe(
+        map(results => {
+            // Combinar los resultados de Tokko y Sanity
+            return [...results.tokko, ...results.sanity]
+        }),
+        catchError(error => {
+            // Manejar el error
+            return throwError(() => new Error(error.message))
+        })
+    )
   }
 
-  getAllFromTokko():Observable<Product[]> {
+  private getAllFromTokko():Observable<Product[]> {
 
     return this.http
             .get<TokkoResponse>(`https://www.tokkobroker.com/api/v1/property/?lang=es_ar&key=${enviroment.tokkoBrokerKey}`)
@@ -63,7 +62,7 @@ export class ProductService {
               }))
   }
 
-  getAllFromSanity():Observable<Product[]> {
+  private getAllFromSanity():Observable<Product[]> {
 
     const encodedQuery = encodeURIComponent(this.filtersService.getSanityQuery())
     return this.http
@@ -81,20 +80,69 @@ export class ProductService {
   }
 
   getById(id: string):Observable<Product> {
+    if(id.length > 10){
+      this.getSanityProductById(id)
+    }
+    return this.getTokkoProductById(id)
+  }
+
+  private getTokkoProductById(id: string): Observable<Product> {
     return this.http
-            .get<TokkoProduct>(`https://www.tokkobroker.com/api/v1/property/${id}/?lang=es_ar&key=${enviroment.tokkoBrokerKey}`)
-            .pipe(
-              catchError(error => {
-                return throwError(() => new Error(error.message))
-              }),
-              map((response) => {
-                // Aquí puedes hacer alguna transformación de los datos si es necesario.
-                return new Product( ).fromTokko(response)
-              }))
+      .get<TokkoProduct>(`https://www.tokkobroker.com/api/v1/property/${id}/?lang=es_ar&key=${enviroment.tokkoBrokerKey}`)
+      .pipe(
+        catchError(error => {
+          return throwError(() => new Error(error.message))
+        }),
+        map((response) => {
+          // Aquí puedes hacer alguna transformación de los datos si es necesario.
+          return new Product().fromTokko(response)
+        }))
+  }
+
+  private getSanityProductById(id: string):Observable<Product> {
+    const query = `*[_id=="${id}"]{
+      ...,
+      operation_type->{
+          title
+      },
+      currency->{
+          title
+      },
+      type->{
+          title
+      },
+      images[]{
+          asset->{
+              path, url
+          }
+      },
+      cover{
+          asset->{
+              path, url
+          }
+      }
+    }`
+    return this.http
+          .get<SanityResponse>(`https://${enviroment.sanityKey}.api.sanity.io/v2022-03-07/data/query/production?query=${query}`)
+          .pipe(
+            catchError(error => {
+              return throwError(() => new Error(error.message))
+            }),
+            map((response) => {
+              // Aquí puedes hacer alguna transformación de los datos si es necesario.
+              console.log(response.result[0])
+              return new Product( ).fromSanity(response.result[0])
+            }))
   }
 
   getSuggested = (id:string) => {
-    console.log(id)
-    return productsMock.slice(0, 3)
+    return this.getAll().pipe(
+      map((products) => {
+        return products.filter((p) => p.id !== id)
+      })/* ,
+      map((products) => {
+        return products.slice(0, 3)
+      }) */
+    )
   }
 }
