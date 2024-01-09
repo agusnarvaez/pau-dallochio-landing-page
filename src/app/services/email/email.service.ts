7cf2a0894597d46c1533dfd7483a007040003f8f
agusnarvaez <agus.narvaez@outlook.com>
2024-01-08 23:10:41 -0300

    Creo service de mailing

---
+++ src/app/services/email/email.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core'
+import { Observable, catchError, map, throwError } from 'rxjs'
+import { enviroment } from '../../../../enviroment.prod'
+import { HttpClient } from '@angular/common/http'
+import { Mail } from '../../models/mail'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  mail = {
+    from: "no_reply@bapps.com.ar",
+    to: "agus.narvaez@outlook.com",
+    subject: "Example Subject",
+    message: "This is the body of the email.",
+    html:"<h1>This is the body of the email</h1><h2>This is the subtitle of the email</h2>"
+  }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  sendEmail(mail:Mail):Observable<any> {
+    console.log('Email sending')
+    return this.http.post<any>(
+      `${enviroment.mail_api_dev}/mail/send`,
+      mail
+      )
+      .pipe(
+        catchError(error => {
+          console.log(error)
+          return throwError(() => new Error(error.message))
+        }),
+        map((response) => {
+          console.log(response)
+          return response
+        }
+      ))
+  }
+
+}
