1ef1c26465ad721774c400aedacc6fb237606517
agusnarvaez <agus.narvaez@outlook.com>
2024-03-13 20:13:15 -0300

    Agrego filtros extra y su búsqueda en tokko

--- src/app/sections/products/products-filter/products-filter.component.ts
+++ src/app/sections/products/products-filter/products-filter.component.ts
@@ -1,35 +1,83 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ButtonComponent } from '../../../components/button/button.component'
-import { FilterObject, FiltersService } from '../../../services/filters/filters.service'
+import {
+  FilterObject,
+  FiltersService,
+} from '../../../services/filters/filters.service'
+import { FormsModule, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-products-filter',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, FormsModule],
   templateUrl: './products-filter.component.html',
-  styleUrl: './products-filter.component.css'
+  styleUrl: './products-filter.component.css',
 })
 export class ProductsFilterComponent {
+  @ViewChild('myForm') myForm!: NgForm
   @Output() filterChange = new EventEmitter<void>()
-
+  type: string = ''
+  rooms: number = 0
+  minPrice: number = 0
+  maxPrice: number = Infinity
   // Propiedades para almacenar el estado de los filtros
   isSellFilterActive: boolean = false
   isRentFilterActive: boolean = false
 
+  showFilters: boolean = false
+  toggleFilters(): void {
+    console.log('toggleFilters')
+    this.showFilters = !this.showFilters
+  }
   constructor(private filtersService: FiltersService) {
     // Inicializar el estado de los filtros
   }
   ngOnInit(): void {
-    this.isSellFilterActive = this.filtersService.isActive("operation_type",'Venta')
-    this.isRentFilterActive = this.filtersService.isActive("operation_type",'Alquiler')
+    this.isSellFilterActive = this.filtersService.isActive(
+      'operation_type',
+      'Venta',
+    )
+    this.isRentFilterActive = this.filtersService.isActive(
+      'operation_type',
+      'Alquiler',
+    )
   }
 
   toggleFilter(filterObj: FilterObject): void {
     this.filtersService.toggle(filterObj)
     // Actualizar el estado de los filtros después de cambiarlos
-    this.isSellFilterActive = this.filtersService.isActive("operation_type",'Venta')
-    this.isRentFilterActive = this.filtersService.isActive("operation_type",'Alquiler')
+    this.isSellFilterActive = this.filtersService.isActive(
+      'operation_type',
+      'Venta',
+    )
+    this.isRentFilterActive = this.filtersService.isActive(
+      'operation_type',
+      'Alquiler',
+    )
+    this.filterChange.emit()
+  }
+  onSubmit(): void {
+    console.log('onSubmit')
+    if (this.myForm.value.type != '')
+      this.filtersService.add({ name: 'type', value: this.myForm.value.type })
+    if (this.myForm.value.rooms > 0)
+      this.filtersService.add({ name: 'rooms', value: this.myForm.value.rooms })
+    if (this.myForm.value.minPrice > 0)
+      this.filtersService.add({
+        name: 'minPrice',
+        value: this.myForm.value.minPrice,
+      })
+    if (this.myForm.value.maxPrice > 0)
+      this.filtersService.add({
+        name: 'maxPrice',
+        value: this.myForm.value.maxPrice,
+      })
+    this.filterChange.emit()
+  }
+  clearFilters(): void {
+    this.myForm.reset()
+    this.filtersService.clear()
     this.filterChange.emit()
   }
 }
--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -1,33 +1,42 @@
 import { Injectable } from '@angular/core'
 
-export interface Filters{
-  [key:string]:string | boolean
+export interface Filters {
+  [key: string]: string | boolean | number
 }
 
 export interface FilterObject {
   name: string
-  value: string | boolean
+  value: string | boolean | number
 }
+type FilterCondition = [string, '=', string | number]
 
+interface TokkoQuery {
+  current_localization_id: number
+  current_localization_type: string
+  price_from: number
+  price_to: number
+  operation_types: number[]
+  property_types: number[]
+  currency: string
+  filters: FilterCondition[]
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FiltersService {
-  private _filters: Filters = {
-  }
+  private _filters: Filters = {}
 
   get = (): Filters => this._filters
-
 
   getSanityQuery(): string {
     const baseQuery = '*[_type == "property"'
     const filterQuery = Object.keys(this._filters)
-      .map(key => {
+      .map((key) => {
         const value = this._filters[key]
         if (typeof value === 'boolean' && value) {
           return ` && ${key} == true`
         } else if (typeof value === 'string') {
-          if(value=='Venta' || value=='Alquiler'){
+          if (value == 'Venta' || value == 'Alquiler') {
             return ` && operation_type->title == "${value}"`
           }
           return ` && ${key} == "${value}"`
@@ -61,23 +70,46 @@ export class FiltersService {
   }
 
   getTokkoQuery(): string {
-    const base_query= {
-      current_localization_id:0,
-      current_localization_type:"country",
-      price_from:0,
-      price_to:999999999,
-      operation_types:[1,2,3],
-      property_types:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-      currency:"ANY",
-      filters:[]
+    const base_query: TokkoQuery = {
+      current_localization_id: 0,
+      current_localization_type: 'country',
+      price_from: 0,
+      price_to: 999999999,
+      operation_types: [1, 2, 3],
+      property_types: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25,
+      ],
+      currency: 'ANY',
+      filters: [],
     }
 
-    if(this._filters["operation_type"]=="Venta") base_query.operation_types=[1]
+    if (this._filters['operation_type'] == 'Venta')
+      base_query.operation_types = [1]
 
-    if(this._filters["operation_type"]=="Alquiler") base_query.operation_types=[2,3]
+    if (this._filters['operation_type'] == 'Alquiler')
+      base_query.operation_types = [2, 3]
 
+    /* if(this._filters['rooms'] > '0') base_query.filters.push(['room_amount', '=', this._filters['rooms']]) */
+
+    if (
+      typeof this._filters['rooms'] === 'number' &&
+      this._filters['rooms'] > 0
+    ) {
+      base_query.filters.push(['room_amount', '=', this._filters['rooms']])
+    }
+
+    if (this._filters['type'] == 'Departamento') base_query.property_types = [2]
+    else if (this._filters['type'] == 'Casa') base_query.property_types = [3]
+    else if (this._filters['type'] == 'PH') base_query.property_types = [13]
+
+    if (this._filters['minPrice'])
+      base_query.price_from = Number(this._filters['minPrice'])
+    if (this._filters['maxPrice'])
+      base_query.price_to = Number(this._filters['maxPrice'])
+
+    console.log(JSON.stringify(base_query))
     return JSON.stringify(base_query)
-
   }
 
   add(filterObj: FilterObject): void {
@@ -92,7 +124,10 @@ export class FiltersService {
     this._filters = {}
   }
 
-  isActive(filterName: string, expectedValue?: string | boolean): boolean {
+  isActive(
+    filterName: string,
+    expectedValue?: string | boolean | number,
+  ): boolean {
     const actualValue = this._filters[filterName]
 
     if (expectedValue === undefined) {
@@ -105,11 +140,10 @@ export class FiltersService {
   }
 
   toggle(filterObj: FilterObject): void {
-    if (this.isActive(filterObj.name,filterObj.value)) {
+    if (this.isActive(filterObj.name, filterObj.value)) {
       this.remove(filterObj)
     } else {
       this.add(filterObj)
     }
-
   }
 }
