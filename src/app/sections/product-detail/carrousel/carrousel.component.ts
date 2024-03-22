9841d0349cc01319953aaf0b95095e0bcc44bddf
agusnarvaez <agus.narvaez@outlook.com>
2024-03-22 17:49:17 -0300

    Agrego touyr 360 a galería de imágenes

--- src/app/sections/product-detail/carrousel/carrousel.component.ts
+++ src/app/sections/product-detail/carrousel/carrousel.component.ts
@@ -7,15 +7,15 @@ import { DomSanitizer } from '@angular/platform-browser'
 @Component({
   selector: 'app-carrousel',
   standalone: true,
-  imports: [CommonModule,ButtonComponent],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './carrousel.component.html',
-  styleUrl: './carrousel.component.css'
+  styleUrl: './carrousel.component.css',
 })
 export class CarrouselComponent {
   @Input() images: string[] | undefined = []
   @Input() videos: string[] | undefined = []
 
-  allMedia = () => [...(this.videos || []),...(this.images || [])]
+  allMedia = () => [...(this.videos || []), ...(this.images || [])]
 
   isVideo = (url: string) => url.includes('youtube') || url.includes('youtu.be')
 
@@ -36,7 +36,6 @@ export class CarrouselComponent {
   //* Esta función se llama cada vez que la ventana cambia de tamaño
   @HostListener('window:resize', ['$event'])
 
-
   //* Esta función se llama cada vez que la ventana cambia de tamaño
   onResize() {
     this.updateCarouselMetrics()
@@ -46,7 +45,7 @@ export class CarrouselComponent {
     if (window.innerWidth >= 530) {
       this.carouselContainerHeight = `${50 * (this.allMedia()?.length || 0)}%` //* 50% * cantidad de imágenes
       this.carouselContainerWidth = '30%' //* 30% del ancho de la ventana
-      this.carouselItemHeight = `${(100 / (this.allMedia()?.length || 1))}%` //* 100% / cantidad de imágenes
+      this.carouselItemHeight = `${100 / (this.allMedia()?.length || 1)}%` //* 100% / cantidad de imágenes
       this.carouselItemWidth = '100%' //* 100% del ancho de la ventana
     } else {
       this.carouselContainerWidth = `${100 * (this.allMedia()?.length || 0)}%` //* 100% * cantidad de imágenes
@@ -55,17 +54,21 @@ export class CarrouselComponent {
       this.carouselItemHeight = '100%' //* 100% del alto de la ventana
     }
   }
+  isRender = (src: string) => src.includes('https://kuula.co/share')
 
-  noImages=() => this.allMedia()?.length === 0
+  noImages = () => this.allMedia()?.length === 0
 
-  selectImage = (index: number) => this.imageIndex = index
+  selectImage = (index: number) => (this.imageIndex = index)
 
   nextImage = () => {
-    if (this.allMedia()) this.imageIndex = (this.imageIndex + 1) % this.allMedia().length
+    if (this.allMedia())
+      this.imageIndex = (this.imageIndex + 1) % this.allMedia().length
   }
 
   prevImage = () => {
-    if (this.allMedia()) this.imageIndex = this.imageIndex === 0 ? this.allMedia().length - 1 : this.imageIndex - 1
+    if (this.allMedia())
+      this.imageIndex =
+        this.imageIndex === 0 ? this.allMedia().length - 1 : this.imageIndex - 1
   }
 
   //* Esta función se usa para saber si una imagen está seleccionada
@@ -78,11 +81,11 @@ export class CarrouselComponent {
   transformStyle = () => {
     if (window.innerWidth >= 530) {
       //* Cambia a transformación vertical
-      const percentage = -((100/this.imagesSize()) * this.imageIndex)
+      const percentage = -((100 / this.imagesSize()) * this.imageIndex)
       return `translateY(${percentage}%)`
     } else {
       //* Mantiene transformación horizontal
-      const percentage = -((100/this.imagesSize()) * this.imageIndex)
+      const percentage = -((100 / this.imagesSize()) * this.imageIndex)
       return `translateX(${percentage}%)`
     }
   }
@@ -91,15 +94,19 @@ export class CarrouselComponent {
   actualImage = () => this.allMedia()?.[this.imageIndex]
 
   //* Esta función se usa para obtener el índice de la imagen actual
-  constructor(private cdr: ChangeDetectorRef, private sanitizer: DomSanitizer) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private sanitizer: DomSanitizer,
+  ) {}
 
   getSafeYoutubeUrl(id: string) {
-    const safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + id)
+    const safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/' + id,
+    )
 
-    console.log(safeUrl)
+    /* console.log(safeUrl) */
     return safeUrl
   }
-
 
   //* Esta función se usa para actualizar el carrusel cuando cambian las imágenes
   ngOnChanges(changes: SimpleChanges) {
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { FiltersService } from '../filters/filters.service'
-import { Observable, catchError, forkJoin, map,throwError } from 'rxjs'
+import { Observable, catchError, forkJoin, map, throwError } from 'rxjs'
 import { Product, SanityProduct, TokkoProduct } from '../../models/product'
 import { HttpClient } from '@angular/common/http'
 import { environment } from '../../../../enviroment.prod'
@@ -12,77 +12,82 @@ interface SanityResponse {
   result: SanityProduct[]
 }
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   filtersService: FiltersService
 
   constructor(
-    filters:FiltersService,
-    private http: HttpClient
-    ) {
+    filters: FiltersService,
+    private http: HttpClient,
+  ) {
     this.filtersService = filters
   }
 
-  filters=( ) => this.filtersService.get()
+  filters = () => this.filtersService.get()
 
-  getAll():Observable<Product[]> {
-
+  getAll(): Observable<Product[]> {
     return forkJoin({
       tokko: this.getAllFromTokko(),
       // TODO: Agregar peticiones de Sanity cuando Pau agregue propiedades
       /* sanity: this.getAllFromSanity() */
     }).pipe(
-        map(results => {
-            // Combinar los resultados de Tokko y Sanity
-            // TODO: Agregar peticiones de Sanity cuando Pau agregue propiedades
-            return [...results.tokko/* , ...results.sanity */]
-        }),
-        catchError(error => {
-            // Manejar el error
-            return throwError(() => new Error(error.message))
-        })
+      map((results) => {
+        // Combinar los resultados de Tokko y Sanity
+        // TODO: Agregar peticiones de Sanity cuando Pau agregue propiedades
+        return [...results.tokko /* , ...results.sanity */]
+      }),
+      catchError((error) => {
+        // Manejar el error
+        return throwError(() => new Error(error.message))
+      }),
     )
   }
 
-  private getAllFromTokko():Observable<Product[]> {
-
+  private getAllFromTokko(): Observable<Product[]> {
     return this.http
-            .get<TokkoResponse>(`https://www.tokkobroker.com/api/v1/property/search?lang=es_ar&key=${environment.tokkoBrokerKey}&data=${this.filtersService.getTokkoQuery()}`)
-            .pipe(
-              catchError(error => {
-                return throwError(() => new Error(error.message))
-              }),
-              map((response) => {
-                // Aquí puedes hacer alguna transformación de los datos si es necesario.
-                return response.objects.map((p) => {
-                  return new Product().fromTokko(p)
-                })
-              }))
+      .get<TokkoResponse>(
+        `https://www.tokkobroker.com/api/v1/property/search?lang=es_ar&key=${
+          environment.tokkoBrokerKey
+        }&data=${this.filtersService.getTokkoQuery()}`,
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(() => new Error(error.message))
+        }),
+        map((response) => {
+          // Aquí puedes hacer alguna transformación de los datos si es necesario.
+          return response.objects.map((p) => {
+            return new Product().fromTokko(p)
+          })
+        }),
+      )
   }
 
-  private getAllFromSanity():Observable<Product[]> {
-
-    const encodedQuery = encodeURIComponent(this.filtersService.getSanityQuery())
+  private getAllFromSanity(): Observable<Product[]> {
+    const encodedQuery = encodeURIComponent(
+      this.filtersService.getSanityQuery(),
+    )
     return this.http
-            .get<SanityResponse>(`https://${environment.sanityKey}.api.sanity.io/v2022-03-07/data/query/production?query=${encodedQuery}`)
-            .pipe(
-              catchError(error => {
-                return throwError(() => new Error(error.message))
-              }),
-              map((response) => {
-                // Aquí puedes hacer alguna transformación de los datos si es necesario.
-                return response.result.map((p) => {
-                  return new Product().fromSanity(p)
-                })
-              }))
+      .get<SanityResponse>(
+        `https://${environment.sanityKey}.api.sanity.io/v2022-03-07/data/query/production?query=${encodedQuery}`,
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(() => new Error(error.message))
+        }),
+        map((response) => {
+          // Aquí puedes hacer alguna transformación de los datos si es necesario.
+          return response.result.map((p) => {
+            return new Product().fromSanity(p)
+          })
+        }),
+      )
   }
 
-  getById(id: string):Observable<Product> {
-    if(id.length > 10){
+  getById(id: string): Observable<Product> {
+    if (id.length > 10) {
       this.getSanityProductById(id)
     }
     return this.getTokkoProductById(id)
@@ -90,18 +95,22 @@ export class ProductService {
 
   private getTokkoProductById(id: string): Observable<Product> {
     return this.http
-      .get<TokkoProduct>(`https://www.tokkobroker.com/api/v1/property/${id}/?lang=es_ar&key=${environment.tokkoBrokerKey}`)
+      .get<TokkoProduct>(
+        `https://www.tokkobroker.com/api/v1/property/${id}/?lang=es_ar&key=${environment.tokkoBrokerKey}`,
+      )
       .pipe(
-        catchError(error => {
+        catchError((error) => {
           return throwError(() => new Error(error.message))
         }),
         map((response) => {
           // Aquí puedes hacer alguna transformación de los datos si es necesario.
+          console.log(response)
           return new Product().fromTokko(response)
-        }))
+        }),
+      )
   }
 
-  private getSanityProductById(id: string):Observable<Product> {
+  private getSanityProductById(id: string): Observable<Product> {
     const query = `*[_id=="${id}"]{
       ...,
       operation_type->{
@@ -125,26 +134,29 @@ export class ProductService {
       }
     }`
     return this.http
-          .get<SanityResponse>(`https://${environment.sanityKey}.api.sanity.io/v2022-03-07/data/query/production?query=${query}`)
-          .pipe(
-            catchError(error => {
-              return throwError(() => new Error(error.message))
-            }),
-            map((response) => {
-              // Aquí puedes hacer alguna transformación de los datos si es necesario.
-              /* console.log(response.result[0]) */
-              return new Product( ).fromSanity(response.result[0])
-            }))
+      .get<SanityResponse>(
+        `https://${environment.sanityKey}.api.sanity.io/v2022-03-07/data/query/production?query=${query}`,
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(() => new Error(error.message))
+        }),
+        map((response) => {
+          // Aquí puedes hacer alguna transformación de los datos si es necesario.
+          /* console.log(response.result[0]) */
+          return new Product().fromSanity(response.result[0])
+        }),
+      )
   }
 
-  getSuggested = (id:string) => {
+  getSuggested = (id: string) => {
     return this.getAll().pipe(
       map((products) => {
         return products.filter((p) => p.id !== id)
-      })/* ,
+      }) /* ,
       map((products) => {
         return products.slice(0, 3)
-      }) */
+      }) */,
     )
   }
 }
