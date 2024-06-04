67ea3cacd8f7bdac08c913e43d6616aabcef5334
agusnarvaez <agus.narvaez@outlook.com>
2024-06-04 19:55:32 -0300

    Agrego separador de miles

--- src/app/sections/home/home-faq/home-faq.component.ts
+++ src/app/sections/home/home-faq/home-faq.component.ts
@@ -5,9 +5,9 @@ import { FaqItemComponent } from '../../../components/faq-item/faq-item.componen
 @Component({
   selector: 'app-home-faq',
   standalone: true,
-  imports: [CommonModule,FaqItemComponent],
+  imports: [CommonModule, FaqItemComponent],
   templateUrl: './home-faq.component.html',
-  styleUrl: './home-faq.component.css'
+  styleUrl: './home-faq.component.css',
 })
 export class HomeFaqComponent {
   constructor() {}
@@ -15,12 +15,13 @@ export class HomeFaqComponent {
     {
       question: '¿Qué documentación necesito para poder publicar mi propiedad?',
       answer:
-        'PARA OPERACIONES DE VENTA: <br>• Título de propiedad, declaratoria de herederos o poder DNI de los titulares, o apoderados <br>• Reglamento de copropiedad (en caso de departamentos o PH) <br>• Plano <br> • Liquidación de expensas <br>• Impuesto municipal <br>• COTI'
+        'PARA OPERACIONES DE VENTA: <br>• Título de propiedad, declaratoria de herederos o poder DNI de los titulares, o apoderados <br>• Reglamento de copropiedad (en caso de departamentos o PH) <br>• Plano <br> • Liquidación de expensas <br>• Impuesto municipal <br>• COTI',
     },
     {
-      question: '¿Cuáles son los gastos estimados en una operación de compraventa?',
+      question:
+        '¿Cuáles son los gastos estimados en una operación de compraventa?',
       answer:
-        'PARA OPERACIONES DE VENTA: <br> • IMPUESTOS: (se abonan en pesos al equivalente del valor del dólar oficial) Retiene el escribano actuante. Se liquidan sobre el valor de escrituración . Sellos 1.75% (por parte), I.T.I. (en caso de corresponder) 1,5%, IVA. <br> • GASTOS DE ESCRIBANÍA: 1-2% (Se solicita presupuesto al escribano actuante) <br> • HONORARIOS INMOBILIARIA: 3%  <br> • IVA (cliente vendedor); 4% <br> • IVA (cliente comprador) <br> • HONORARIOS ESCRIBANÍA: Solo le corresponden al cliente comprador'
+        'PARA OPERACIONES DE VENTA: IMPUESTOS: (se abonan en pesos al equivalente del valor del dólar oficial) Retiene el escribano actuante. Se liquidan sobre el valor de escrituración + Sellos 1.75% (por parte), I.T.I. (en caso de corresponder) 1,5%, IVA. <br> - GASTOS DE ESCRIBANÍA: 1-2% (Se solicita presupuesto al escribano actuante) - HONORARIOS INMOBILIARIA: 3%  <br> • IVA (cliente vendedor); 4% + IVA (cliente comprador) <br> • HONORARIOS ESCRIBANÍA: Solo le corresponden al cliente comprador',
     },
     /* {
       question: '¿Cuál es la inversión inicial para un alquiler?',
@@ -29,13 +30,14 @@ export class HomeFaqComponent {
     }, */
     {
       question: '¿Cómo calculo el índice de ajuste de mi alquiler?',
-      answer: '<a href="https://cpicalq.portalcucicba.com.ar/calculo_add.php" target="_blank" rel="noreferer" >CALCULADORA DE AJUSTE</a>'
+      answer:
+        '<a href="https://cpicalq.portalcucicba.com.ar/calculo_add.php" target="_blank" rel="noreferer" >CALCULADORA DE AJUSTE</a>',
     },
     {
-      question: '¿Cómo preparo mi casa para el relevamiento multimedia y las visitas?',
+      question:
+        '¿Cómo preparo mi casa para el relevamiento multimedia y las visitas?',
       answer:
-        'El Home staging o puesta en escena, es la forma en que se prepara tu inmueble para que se luzca en la publicación y las visitas de los interesados.  Sugerimos: <br> •	Despejar muebles y ambientes <br>•	Despersonalizar muros, quitar fotos e imágenes religiosas <br>•	Mejorar el atractivo exterior <br>•	Darle estilo a la mesa del comedor <br>•	Generar un espacio de relax al aire libre <br>•	Prestar atención a la estación del año <br>•	Limpiar y ventilar <br>•	Guardar cualquier juguete u objeto que pueda estar sobre el piso <br>•	Aromatizar sutilmente'
-    }
+        'El Home staging o puesta en escena, es la forma en que se prepara tu inmueble para que se luzca en la publicación y las visitas de los interesados.  Sugerimos: <br> •	Despejar muebles y ambientes <br>•	Despersonalizar muros, quitar fotos e imágenes religiosas <br>•	Mejorar el atractivo exterior <br>•	Darle estilo a la mesa del comedor <br>•	Generar un espacio de relax al aire libre <br>•	Prestar atención a la estación del año <br>•	Limpiar y ventilar <br>•	Guardar cualquier juguete u objeto que pueda estar sobre el piso <br>•	Aromatizar sutilmente',
+    },
   ]
-
 }
--- src/app/sections/products/products-card/products-card.component.ts
+++ src/app/sections/products/products-card/products-card.component.ts
@@ -2,12 +2,20 @@ import { Component, Input } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { Product } from '../../../models/product'
 import { RouterLink } from '@angular/router'
+
+// Importa locales adicionales
+import localeEs from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common'
+
+// Registra los datos locales
+registerLocaleData(localeEs)
 @Component({
   selector: 'app-products-card',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  providers: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './products-card.component.html',
-  styleUrl: './products-card.component.css'
+  styleUrl: './products-card.component.css',
 })
 export class ProductsCardComponent {
   @Input() product?: Product
