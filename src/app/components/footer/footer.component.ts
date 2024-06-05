a7bef936f5a8e19d6536a6d00f6de145ef828efd
agusnarvaez <agus.narvaez@outlook.com>
2024-06-05 01:00:32 -0300

    Corrijo links de footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
