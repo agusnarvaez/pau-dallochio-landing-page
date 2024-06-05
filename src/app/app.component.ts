a58952d730fe84723ded9c03f25f0203c9a68d80
agusnarvaez <agus.narvaez@outlook.com>
2024-06-05 00:38:34 -0300

    Prueba 1 de scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { RouterOutlet } from '@angular/router'
-import { HeaderComponent } from "./components/header/header.component"
-import { FooterComponent } from "./components/footer/footer.component"
-
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { filter } from 'rxjs'
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   show = false
   title = 'pau-dallochio-landing-page'
+
+  constructor(private router: Router) {
+    // Escucha los eventos de navegación finalizada
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo(0, 0)
+      })
+  }
 }
